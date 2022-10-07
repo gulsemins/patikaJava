7f6d228b73b1f69ed709c82804277706d79bb6f6
gulsemins <gulsemin111@gmail.com>
2022-10-07 15:23:19 +0300

    added

---
+++ hava SicakliginaGoreOnerme/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+        int heat;
+
+        System.out.print("Please enter a temperature value: ");
+        heat = input.nextInt();
+
+        if (heat < 5) {
+            System.out.print("Kayak yapabilirsiniz.");
+        } else if ( heat <= 25 ) {
+            if (heat <= 15) {
+                System.out.println("Sinemaya gidebilirsiniz.");
+            }
+            if ( heat >= 10 ) {
+                System.out.println("Piknik yapmaya gidebilirsiniz.");
+            }
+        } else {
+            System.out.println("Yüzmeye gidebilirsiniz.");
+        }
+	}
+
+}
